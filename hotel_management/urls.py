d35f0b21a9bd796b27e02e5b8da15bfe9e49a1d3
Dev Sehgal <83958063+DevSTheDeveloper@users.noreply.github.com>
2024-01-23 09:30:13 +0000

    Added LOG OUT Button

--- hotel_management/urls.py
+++ hotel_management/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views  # Import Django's authentication views
 from hotel.views import login_view, redirect_to_rooms, redirect_to_homepage
 from hotel import views
 
@@ -9,10 +10,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('homepage/', TemplateView.as_view(template_name='homepage.html'), name='homepage'),
     path('login/', login_view, name='login_view'),
-    path('rooms/', views.rooms_views, name='rooms'),  # Updated view function name and URL pattern
+    path('rooms/', views.rooms_views, name='rooms'),
     path('redirect-to-homepage/', redirect_to_homepage, name='redirect_to_homepage'),
     path('update_room_status/', views.update_room_status, name='update_room_status'),
     path('reservations/', views.make_reservation, name='make_reservation'),
     path('guests/', TemplateView.as_view(template_name='guest.html'), name='guest_page'),
 
+    # Add the URL pattern for logout
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
